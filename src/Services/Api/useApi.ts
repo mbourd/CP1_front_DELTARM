0de5e1520da60c25d8d3186c139708edd4c70cc7
Valentin Berlier <berlier.v@gmail.com>
2020-12-09 11:05:39 +0100

    feat: authenticate requests when there is a persisted user

--- src/Services/Api/useApi.ts
+++ src/Services/Api/useApi.ts
@@ -1,6 +1,14 @@
 import { useApi as DRMUseApi, UseApiReturnType, apiRouter } from 'Packages/Api';
+import { security } from 'Packages/Security';
+import { useEffect } from 'react';
 import { getEnv } from 'Services';
 
 export const useApi = <T>(promise = false): UseApiReturnType<T> => {
-  return DRMUseApi<T>(getEnv('API_HOST'), getEnv('API_PROTOCOL'), apiRouter, promise);
+  const { request, ...api } = DRMUseApi<T>(getEnv('API_HOST'), getEnv('API_PROTOCOL'), apiRouter, promise);
+
+  useEffect(() => {
+    request.setJWT(security.getUser().getJwt());
+  }, [request]);
+
+  return { request, ...api };
 };
