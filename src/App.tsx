41ecb2d8e15eb5f73a8627539dcd389ce3f5faf2
Franck DIOMANDE <fkdiomande@gmail.com>
2020-12-11 01:39:34 +0100

    fix: add header

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,11 @@ import 'Services/Api/registerCallState';
 import { MainHeader } from 'Shared/components';
 import { MainContent } from 'Shared/components';
 import { security, SecurityProvider } from 'Services';
-import { useSecurity } from './Packages/Security';
 
 const App = (): React.ReactElement => {
-  const { user } = useSecurity();
-
   return (
     <SecurityProvider security={security}>
-      {user.isLogged() ? <MainHeader /> : null}
+      <MainHeader />
       <MainContent />
     </SecurityProvider>
   );
