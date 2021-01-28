eb654c2fd8b9e2459fc8d3b2a9036c9ac6a6cc8f
Valentin Berlier <berlier.v@gmail.com>
2021-01-28 17:47:07 +0100

    fix: relative control footer wrapper

--- src/Features/Edit/components/Control/Form/ControlFooter.tsx
+++ src/Features/Edit/components/Control/Form/ControlFooter.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Box, Grid } from '@material-ui/core';
 import { BPITooltip } from 'Shared/components';
 import { UserCheckedIcon, WarningIcon } from 'Styles';
 import { IControl } from 'Features/Edit/types';
@@ -10,25 +10,27 @@ interface IProps {
 
 export const ControlFooter: React.FC<IProps> = ({ control }): React.ReactElement => {
   return (
-    <Grid className={'control-footer'} container component={'span'} alignItems={'center'} wrap={'nowrap'}>
-      <Grid item component={'span'}>
-        {control.previousValue ? (
-          <BPITooltip title={control.previousValue}>
-            <span>
-              <UserCheckedIcon fontSize={'small'} />
-            </span>
-          </BPITooltip>
-        ) : null}
+    <Box position="relative">
+      <Grid className={'control-footer'} container component={'span'} alignItems={'center'} wrap={'nowrap'}>
+        <Grid item component={'span'}>
+          {control.previousValue ? (
+            <BPITooltip title={control.previousValue}>
+              <span>
+                <UserCheckedIcon fontSize={'small'} />
+              </span>
+            </BPITooltip>
+          ) : null}
+        </Grid>
+        <Grid item component={'span'}>
+          {control.desc2 ? (
+            <BPITooltip title={control.desc2}>
+              <span>
+                <WarningIcon fontSize={'small'} />
+              </span>
+            </BPITooltip>
+          ) : null}
+        </Grid>
       </Grid>
-      <Grid item component={'span'}>
-        {control.desc2 ? (
-          <BPITooltip title={control.desc2}>
-            <span>
-              <WarningIcon fontSize={'small'} />
-            </span>
-          </BPITooltip>
-        ) : null}
-      </Grid>
-    </Grid>
+    </Box>
   );
 };
