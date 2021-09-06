c64faff364dd16dc90c9dfb78c4e1a1dddd4e0fb
Nicolas Teysseyre <nicolas.teysseyre@deltarm.com>
2021-09-06 10:23:48 +0200

    fix: waiting connection

--- src/Features/Login/components/Login.tsx
+++ src/Features/Login/components/Login.tsx
@@ -5,11 +5,12 @@ import { useLocation } from 'react-router-dom';
 import './translations';
 import { LoginStyled } from './Login.style';
 import { HeadingOne, PageLoader } from 'Shared/components';
-import { useTrans, SecurityContext } from 'Services';
+import { useTrans, SecurityContext, useSecurity } from 'Services';
 
 const Login: React.FC = (): React.ReactElement => {
   const [trans] = useTrans('Login');
   const { login } = useContext(SecurityContext);
+  const { user } = useSecurity();
 
   const location = useLocation();
 
@@ -22,6 +23,12 @@ const Login: React.FC = (): React.ReactElement => {
       login(token);
     }
   }, [login, token]);
+  useEffect(() => {
+    if (user.isLogged()) {
+      // We need the updated state immediately available
+      window.location.href = '/';
+    }
+  }, [user]);
 
   return (
     <LoginStyled>
--- src/Services/Security/SecurityProvider.tsx
+++ src/Services/Security/SecurityProvider.tsx
@@ -29,7 +29,7 @@ export const SecurityProvider: React.FC<ISecurityProviderProps> = ({ security, c
   const [user, setUser] = useState<IUser>(security.getUser());
   const jwt = user.getJwt();
 
-  const { send, request } = useApi<unknown>({ promise: true });
+  const { send, request } = useApi<void>({ promise: true });
   request.setBearerToken(jwt);
 
   const login = useCallback(
@@ -39,13 +39,12 @@ export const SecurityProvider: React.FC<ISecurityProviderProps> = ({ security, c
         const user = new User();
         user.fromJwt(body.data.jwt);
         security.persistUser(user);
-        await setUser(user);
-        router.redirectTo('dashboard');
+        setUser(user);
       } catch {
         router.redirectTo('loginError');
       }
     },
-    [send, security],
+    [send, security, setUser],
   );
 
   const logout = useCallback(() => {
@@ -54,7 +53,7 @@ export const SecurityProvider: React.FC<ISecurityProviderProps> = ({ security, c
       setUser(new User());
     }
     window.location.href = getEnv('LOGOUT_REDIRECT');
-  }, [security, user]);
+  }, [security, user, setUser]);
 
   const context = useMemo(
     () => ({
@@ -86,7 +85,7 @@ export const SecurityProvider: React.FC<ISecurityProviderProps> = ({ security, c
         clearTimeout(timeout);
       }
     };
-  }, [send, jwt, context.data]);
+  }, [send, jwt, context.data, setUser]);
 
   const idleTimeout = parseInt(getEnv('IDLE_TIMEOUT')) * 1000;
 
