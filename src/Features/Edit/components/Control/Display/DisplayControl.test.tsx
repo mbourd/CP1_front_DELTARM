0dded93d750ca290ba14438275ee4b4dcb3bed46
Nicolas Teysseyre <nicolas.teysseyre@deltarm.com>
2021-11-08 14:15:23 +0100

    feat: state for the display control method

--- src/Features/Edit/components/Control/Display/DisplayControl.test.tsx
+++ src/Features/Edit/components/Control/Display/DisplayControl.test.tsx
@@ -5,7 +5,7 @@ import { render } from '@testing-library/react';
 // import userEvent from '@testing-library/user-event';
 import { ThemeProvider } from 'styled-components/macro';
 import { BPITheme } from '../../../../../Packages/Design';
-
+import { DisplayControl } from './DisplayControl';
 const { TextControl, UploadFileControl, EmailControl } = composeStories(
   stories,
 );
@@ -13,17 +13,22 @@ const { TextControl, UploadFileControl, EmailControl } = composeStories(
 describe('DisplayControl', () => {
   describe('Display Control', () => {
     test('Should render the controls', async () => {
-      // why theme is not hanlded by preview.js ?
+      // why theme is not handled by preview.js ?
       render(
         <ThemeProvider theme={BPITheme}>
           <TextControl />
-          <UploadFileControl />
           <EmailControl />
+          <UploadFileControl />
         </ThemeProvider>,
       );
+      // expect(wrapper.instance()).toBeCalled();
       // userEvent.click(getByText('Click me!'));
       // https://testing-library.com/docs/queries/about/
       // expect.toBeInTheDocument();
     });
+
+    // it('has an initial state', () => {
+    //   expect(wrapper.state()).toBeValid();
+    // });
   });
 });
--- src/Features/Edit/components/Control/Display/DisplayControl.tsx
+++ src/Features/Edit/components/Control/Display/DisplayControl.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Grid } from '@material-ui/core';
 import { DisplayControlStyled } from './DisplayControl.style';
 import { IControl } from 'Features/Edit/types';
@@ -11,17 +11,21 @@ export interface IProps {
 export const DisplayControl: React.FC<IProps> = ({
   controls,
 }): React.ReactElement => {
-  // const [formState, setFormState] = useState(controls);
-
+  const [formState, setFormState] = useState(controls);
   // const formStateWithDisabled = injectDisabled(formState)
-
-  console.log('CONTROLS', controls);
 
   return (
     <DisplayControlStyled>
       <Grid container className={'control-container'}>
-        {controls.map((control, index) => {
-          return <SwitchControlItem key={index} control={control} />;
+        {formState.map((control, index) => {
+          return (
+            <SwitchControlItem
+              key={index}
+              control={control}
+              formState={formState}
+              setFormState={setFormState}
+            />
+          );
         })}
       </Grid>
     </DisplayControlStyled>
