cb26fc7157f6342614318186169213b9ab0b3428
Nicolas Teysseyre <nicolas.teysseyre@deltarm.com>
2021-11-19 10:36:39 +0100

    fix: padding header dashboard

--- src/Shared/components/Heading/HeadingDynamic/Heading.style.ts
+++ src/Shared/components/Heading/HeadingDynamic/Heading.style.ts
@@ -10,6 +10,6 @@ export const HeadingStyled = styled.h1<IProps>`
   font-family: ${({ theme }) => theme.font.heading.main};
   font-size: ${({ theme }) => theme.sizing.heading.one};
   line-height: 40px;
-  margin: 3rem;
+  margin: 1rem;
   text-align: center;
 `;
