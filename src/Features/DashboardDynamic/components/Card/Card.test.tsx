6b284bde720892b440ca5adfa248efef9a4a9437
Nicolas Teysseyre <nicolas.teysseyre@deltarm.com>
2021-11-23 16:02:07 +0100

    feat: add tests for card

--- src/Features/DashboardDynamic/components/Card/Card.test.tsx
+++ src/Features/DashboardDynamic/components/Card/Card.test.tsx
@@ -8,10 +8,25 @@ describe('Card Dashboard', () => {
   // describe is a test suite
   describe('Display dashboard', () => {
     // test is a test case
-    test('Should display text on card dashboard', async () => {
+    test('Should display header on card dashboard', async () => {
       const { getByText, getByRole } = render(<CardDashboard />);
       expect(getByRole('banner')).toBeInTheDocument();
       expect(getByText('4 Contrôles à réaliser')).toBeInTheDocument();
+    });
+    test('Should display table header on card dashboard', async () => {
+      const { getByText } = render(<CardDashboard />);
+      expect(getByText('Etat')).toBeInTheDocument();
+      expect(getByText('+')).toBeInTheDocument();
+      expect(getByText('Fini')).toBeInTheDocument();
+      expect(getByText('BI')).toBeInTheDocument();
+    });
+    test('Should display content of cells on card dashboard', async () => {
+      const { getByText } = render(<CardDashboard />);
+      expect(getByText('Site A - M1')).toBeInTheDocument();
+    });
+    test('Should target a new window when click on icon with target blank', async () => {
+      const { getByText } = render(<CardDashboard />);
+      expect(getByText('Site A - M1')).toBeInTheDocument();
     });
   });
 });
