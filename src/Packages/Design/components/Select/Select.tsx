01ac15b299f6db8a313b9d90a19076ea1b81073b
Nicolas Teysseyre <nicolas.teysseyre@deltarm.com>
2021-12-09 19:11:36 +0100

    fix: select list from mui

--- src/Packages/Design/components/Select/Select.tsx
+++ src/Packages/Design/components/Select/Select.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { ClickAwayListener } from '@material-ui/core';
+import { ClickAwayListener } from '@mui/material';
 import { SelectStyled } from './Select.style';
 import { ISelect, ISelectData } from './types';
 import { SelectLabel } from './Label/SelectLabel';
