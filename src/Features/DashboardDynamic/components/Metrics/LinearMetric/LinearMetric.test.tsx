e8d3cbdc0f6d33303c4e66708e9342500a834fb9
Nicolas Teysseyre <nicolas.teysseyre@deltarm.com>
2021-12-29 15:09:12 +0100

    feat: unit test for metrics

--- src/Features/DashboardDynamic/components/Metrics/LinearMetric/LinearMetric.test.tsx
+++ src/Features/DashboardDynamic/components/Metrics/LinearMetric/LinearMetric.test.tsx
@@ -7,7 +7,14 @@ const { LinearIndicator } = composeStories(stories);
 describe('Linear Metric', () => {
   describe('metric', () => {
     test('Should render the linear metric', async () => {
-      render(<LinearIndicator />);
+      const { getByRole } = render(<LinearIndicator />);
+      expect(getByRole('progressbar')).toBeInTheDocument();
+      expect(getByRole('progressbar')).toHaveAttribute('role', 'progressbar');
+      expect(getByRole('progressbar')).toHaveAttribute(
+        'title',
+        'Taux de Complétude : 54 dossiers / 100 prévus au plan = 54%',
+      );
+      expect(getByRole('progressbar')).toHaveAttribute('aria-valuenow', '54');
     });
   });
 });
