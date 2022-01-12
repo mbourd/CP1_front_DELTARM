12b85e0ecc09b8a3bb7c6ee1bec5b3c51611f331
Nicolas Teysseyre <nicolas.teysseyre@deltarm.com>
2022-01-12 09:52:12 +0100

    feat: decimal control

--- src/Features/Edit/components/Control/Form/index.ts
+++ src/Features/Edit/components/Control/Form/index.ts
@@ -10,3 +10,4 @@ export * from './Checkbox/CheckboxControl';
 export * from './Upload/UploadControl';
 export * from './Compliance/CheckboxCompliance/CheckboxCompliance';
 export * from './InfoBlock/InfoBlockControl';
+export * from './Decimal/DecimalControl';
