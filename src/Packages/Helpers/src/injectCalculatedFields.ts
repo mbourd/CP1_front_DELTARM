1653ed491b42737e3618f83b6adc08caf5267500
Nicolas Teysseyre <nicolas.teysseyre@deltarm.com>
2022-01-13 15:44:46 +0100

    feat: calculations and map values of list fields

--- src/Packages/Helpers/src/injectCalculatedFields.ts
+++ src/Packages/Helpers/src/injectCalculatedFields.ts
@@ -11,19 +11,36 @@ export const injectCalculatedFields = (
       let formula = field.formula.formula;
 
       fieldsToReplaceInFormula?.map((fieldToReplace) => {
-        const valueToReplace = formState.find(
+        const foundedField = formState.find(
           (fieldToFind) => '#' + fieldToFind.control_id === fieldToReplace,
         );
-        if (valueToReplace) {
+
+        if (foundedField && !foundedField?.control_answer_choices) {
           formula = formula.replaceAll(
             fieldToReplace,
-            `${valueToReplace.control_value}`,
+            `${foundedField.control_value}`,
+          );
+        }
+
+        if (foundedField?.control_answer_choices) {
+          const value = foundedField.control_answer_choices.find(
+            (choice) => '' + choice.choice_id === foundedField.control_value,
+          );
+          formula = formula.replaceAll(
+            fieldToReplace,
+            `${value?.choice_value}`,
           );
         }
       });
-
       // eslint-disable-next-line no-eval
       field.control_value = eval(`${formula}`).toString();
+    }
+    if (field.formula?.map) {
+      field.formula.map.map((map) => {
+        if (between(field.control_value, map.min, map.max)) {
+          field.control_value = map.lib;
+        }
+      });
     }
   });
 
