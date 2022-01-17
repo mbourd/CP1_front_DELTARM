a28844e52ec8a376b0fb185863599f55398590fe
Nicolas Teysseyre <nicolas.teysseyre@deltarm.com>
2022-01-17 16:18:23 +0100

    feat: mock datas for search bar dynamic

--- src/Features/DashboardDynamic/components/DashboardDynamic.stories.tsx
+++ src/Features/DashboardDynamic/components/DashboardDynamic.stories.tsx
@@ -3,6 +3,7 @@ import { DashboardDynamic } from './DashboardDynamic';
 import { worker } from '../../../mocks/server';
 import { rest } from 'msw';
 import data from '../../../mocks/fixtures/dashboard/dashboard';
+import dataModal from '../../../mocks/fixtures/modal/modal';
 import { RecoilRoot } from 'recoil';
 
 export default {
@@ -23,6 +24,12 @@ const Template: any = (args: any) => {
       'https://controle-api-dev.deltarm.com:8082/dashboard/contr_perm',
       (req, res, ctx) => {
         return res(ctx.status(200), ctx.json(data));
+      },
+    ),
+    rest.get(
+      'https://controle-api-dev.deltarm.com:8082/contr_perm/get_search_test?value=',
+      (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json(dataModal));
       },
     ),
   );
--- src/Features/DashboardDynamic/components/Search/SearchBar.stories.tsx
+++ src/Features/DashboardDynamic/components/Search/SearchBar.stories.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import { SearchBar } from './SearchBar';
+import { worker } from '../../../../mocks/server';
+import { rest } from 'msw';
+import data from '../../../../mocks/fixtures/modal/modal';
+import { RecoilRoot } from 'recoil';
 
 export default {
   title: 'SearchBar',
   component: SearchBar,
-  decorators: [(story: any) => <div className="p-6">{story()}</div>],
+  decorators: [
+    (story: any) => (
+      <RecoilRoot>
+        <div className="p-6">{story()}</div>
+      </RecoilRoot>
+    ),
+  ],
 };
 
-const Template: any = (args: any) => <SearchBar {...args} />;
+const Template: any = (args: any) => {
+  worker?.use(
+    rest.get(
+      'https://controle-api-dev.deltarm.com:8082/contr_perm/get_search_test?value=',
+      (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json(data));
+      },
+    ),
+  );
+
+  return <SearchBar {...args} />;
+};
 
 export const Search = Template.bind({});
 Search.args = {
@@ -19,21 +40,33 @@ Search.args = {
       regex: '[0-9A-Za-z]\\/[0-9A-Za-z]',
       regex_msg:
         'La recherche doit respecter le format : N° de Dossier / Avenant',
-      route: '/file/search',
+      action: {
+        endpoint: '/contr_perm/get_search_test?value=',
+        method: 'GET',
+        params: null,
+      },
     },
     {
       lib: "Rechercher par nom d'utilisateur",
       placeholder: "Nom de l'utilisateur",
       regex: null,
       regex_msg: null,
-      route: '/file/search_name',
+      action: {
+        endpoint: '/contr_perm/get_search_test?value=',
+        method: 'GET',
+        params: null,
+      },
     },
     {
       lib: 'Recherche Globale',
       placeholder: 'Chaîne de caractère à rechercher',
       regex: null,
       regex_msg: null,
-      route: '/file/search_global',
+      action: {
+        endpoint: '/contr_perm/get_search_test?value=',
+        method: 'GET',
+        params: null,
+      },
     },
   ],
 };
