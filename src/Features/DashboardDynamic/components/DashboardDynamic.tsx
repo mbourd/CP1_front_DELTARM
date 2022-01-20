ae3e742054f5512e90acbad4856fbdffd898d5e1
Nicolas Teysseyre <nicolas.teysseyre@deltarm.com>
2022-01-20 11:11:09 +0100

    feat: fallback ui and loader on dashboard dynamic

--- src/Features/DashboardDynamic/components/DashboardDynamic.tsx
+++ src/Features/DashboardDynamic/components/DashboardDynamic.tsx
@@ -1,7 +1,12 @@
-import React, { Suspense, useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Grid } from '@material-ui/core';
 
-import { useApi, useSecurity, SecurityContext } from 'Services';
+import {
+  useApi,
+  useSecurity,
+  SecurityContext,
+  SwitchCallState,
+} from 'Services';
 import { BreadCrumb, Heading } from 'Shared/components';
 import { DashboardStyled, MetricsContainerStyled } from './Dashboard.style';
 import { ButtonContainerStyled } from './Dashboard.style';
@@ -18,7 +23,7 @@ import { IDataModal } from '../../ModalDynamic/components/types';
 import { NoData } from './NoData';
 
 const DashboardDynamic: React.FC = (): React.ReactElement => {
-  const { send, data: response } = useApi<IDashboard>();
+  const { send, data: response, callState } = useApi<IDashboard>();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const { user } = useSecurity();
   const jwt = user.getJwt();
@@ -40,81 +45,84 @@ const DashboardDynamic: React.FC = (): React.ReactElement => {
 
   return (
     <>
-      <Suspense fallback={<IsLoading />}>
+      <SwitchCallState
+        callState={callState}
+        states={{
+          IS_LOADING: <IsLoading />,
+          NO_DATA: <NoData />,
+          SERVER_ERROR: <NoData />,
+        }}
+      >
         <BreadCrumb values={['Dashboard']} />
-        {response?.data ? (
-          <DashboardStyled>
-            {response?.data.title.visible && (
-              <Heading
-                style={{
-                  fontSize: response?.data.title.font_size,
-                  color: response?.data.title.font_color,
-                }}
-              >
-                {response?.data.title.lib}
-              </Heading>
-            )}
-            {response?.data.subtitle.visible && (
-              <Heading
-                style={{
-                  fontSize: response?.data.subtitle.font_size,
-                  color: response?.data.subtitle.font_color,
-                }}
-              >
-                {response?.data.subtitle.lib}
-              </Heading>
-            )}
-            {response?.data.search_bar.search_bar && (
-              <SearchBar
-                btn_lib={response?.data.search_bar.btn_lib}
-                options={response?.data.search_bar.options}
-                setIsModalOpen={setIsModalOpen}
-              />
-            )}
-            {response && response?.data.btns.length > 0 ? (
-              <ButtonContainerStyled>
-                {response?.data.btns.map((btn, index) => {
-                  return (
-                    <Button
-                      key={index}
-                      onClick={() => actionButton(btn.action)}
-                      style={{ backgroundColor: btn.bg_color }}
-                    >
-                      {btn.btn_lib}
-                    </Button>
-                  );
-                })}
-              </ButtonContainerStyled>
-            ) : null}
-            <MetricsContainerStyled>
-              <Grid container component={'span'} alignItems={'center'}>
-                {response?.data.metrics.visible
-                  ? response?.data.metrics.indicator.map((indicator, index) => (
-                      <SwitchMetric indicator={indicator} key={index} />
-                    ))
-                  : null}
-              </Grid>
-            </MetricsContainerStyled>
-            <Grid container>
-              {response?.data.cards.visible &&
-                response?.data.cards.card.map((card, index) => (
-                  <Grid item xs={12} md={6} key={index}>
-                    <Card card={card} key={index} actionIcons={actionButton} />
-                  </Grid>
-                ))}
+        <DashboardStyled>
+          {response?.data.title.visible && (
+            <Heading
+              style={{
+                fontSize: response?.data.title.font_size,
+                color: response?.data.title.font_color,
+              }}
+            >
+              {response?.data.title.lib}
+            </Heading>
+          )}
+          {response?.data.subtitle.visible && (
+            <Heading
+              style={{
+                fontSize: response?.data.subtitle.font_size,
+                color: response?.data.subtitle.font_color,
+              }}
+            >
+              {response?.data.subtitle.lib}
+            </Heading>
+          )}
+          {response?.data.search_bar.search_bar && (
+            <SearchBar
+              btn_lib={response?.data.search_bar.btn_lib}
+              options={response?.data.search_bar.options}
+              setIsModalOpen={setIsModalOpen}
+            />
+          )}
+          {response && response?.data.btns.length > 0 ? (
+            <ButtonContainerStyled>
+              {response?.data.btns.map((btn, index) => {
+                return (
+                  <Button
+                    key={index}
+                    onClick={() => actionButton(btn.action)}
+                    style={{ backgroundColor: btn.bg_color }}
+                  >
+                    {btn.btn_lib}
+                  </Button>
+                );
+              })}
+            </ButtonContainerStyled>
+          ) : null}
+          <MetricsContainerStyled>
+            <Grid container component={'span'} alignItems={'center'}>
+              {response?.data.metrics.visible
+                ? response?.data.metrics.indicator.map((indicator, index) => (
+                    <SwitchMetric indicator={indicator} key={index} />
+                  ))
+                : null}
             </Grid>
-            {isModalOpen && modal ? (
-              <ModalDynamic
-                open={isModalOpen}
-                setIsModalOpen={setIsModalOpen}
-                data={modal}
-              />
-            ) : null}
-          </DashboardStyled>
-        ) : (
-          <NoData />
-        )}
-      </Suspense>
+          </MetricsContainerStyled>
+          <Grid container>
+            {response?.data.cards.visible &&
+              response?.data.cards.card.map((card, index) => (
+                <Grid item xs={12} md={6} key={index}>
+                  <Card card={card} key={index} actionIcons={actionButton} />
+                </Grid>
+              ))}
+          </Grid>
+          {isModalOpen && modal ? (
+            <ModalDynamic
+              open={isModalOpen}
+              setIsModalOpen={setIsModalOpen}
+              data={modal}
+            />
+          ) : null}
+        </DashboardStyled>
+      </SwitchCallState>
     </>
   );
 };
