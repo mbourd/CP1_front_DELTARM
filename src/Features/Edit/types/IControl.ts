9dc40e69c27de1614963a821c4202ac968549d80
Nicolas Teysseyre <nicolas.teysseyre@deltarm.com>
2022-04-06 10:43:21 +0200

    fix: duplicate key calculatedValue interface Icontrol

--- src/Features/Edit/types/IControl.ts
+++ src/Features/Edit/types/IControl.ts
@@ -227,5 +227,4 @@ export interface IApiControl {
   control_rejectable: IAPIControlRejectable | null;
   useRejection?: ControlRejectable;
   data_grid_detail?: DataGridDetail | null;
-  calculatedValue?: string;
 }
