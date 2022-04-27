06b450455827a949a48ac1957ac5d4024a4ddb74
Nicolas Teysseyre <nicolas.teysseyre@deltarm.com>
2022-04-27 11:21:27 +0200

    feat: add icon to download xls file audit

--- src/Features/Audit/File/FileAudit.tsx
+++ src/Features/Audit/File/FileAudit.tsx
@@ -1,17 +1,29 @@
-import React, { useContext, useEffect, useRef } from 'react';
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import { Card } from '@material-ui/core';
-import { BPIBadge, Popper } from 'Shared/components';
+import { BPIBadge, FormError, Popper } from 'Shared/components';
 import { AuditIcon } from 'Styles';
-import { useApi } from 'Services';
+import { IUser, security, useApi } from 'Services';
 import { EditValidationContext } from 'Features/Edit';
 import { IFileAudit } from '../types';
 import { FileAuditBody } from './Body/FileAuditBody';
 import { FileAuditStyled, FileAuditHeaderStyled } from './FileAudit.style';
+import { ExcelIcon } from '../../../Packages/Design/icons/ExcelIcon';
+import { downloadAuditExcel } from './downloadAuditExcel';
+
 export const FileAudit: React.FC = (): React.ReactElement => {
   const [anchorEl, setAnchorEl] = React.useState<SVGSVGElement | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const { request, send, data } = useApi<IFileAudit[]>();
   const context = useContext(EditValidationContext);
   const { fileId } = context;
+  const [user] = useState<IUser>(security.getUser());
+  const jwt = user.getJwt();
 
   const iconRef = useRef<Element | null>(null);
 
@@ -22,6 +34,14 @@ export const FileAudit: React.FC = (): React.ReactElement => {
       request.abort();
     };
   }, [send, fileId, request]);
+
+  const handleDownloadExcelAudit = useCallback(
+    (e) => {
+      e.preventDefault();
+      downloadAuditExcel(fileId, jwt, setErrorMessage);
+    },
+    [jwt, fileId],
+  );
 
   return (
     <>
@@ -48,10 +68,22 @@ export const FileAudit: React.FC = (): React.ReactElement => {
       >
         <FileAuditStyled>
           <Card>
-            <FileAuditHeaderStyled>Audit du dossier</FileAuditHeaderStyled>
+            <FileAuditHeaderStyled>
+              Audit du dossier{' '}
+              <ExcelIcon
+                style={{ float: 'right' }}
+                fontSize={'medium'}
+                onClick={handleDownloadExcelAudit}
+              />
+            </FileAuditHeaderStyled>
             {data ? <FileAuditBody audits={data} /> : null}
           </Card>
         </FileAuditStyled>
+        {errorMessage ? (
+          <p>
+            <FormError>{errorMessage}</FormError>
+          </p>
+        ) : null}
       </Popper>
     </>
   );
