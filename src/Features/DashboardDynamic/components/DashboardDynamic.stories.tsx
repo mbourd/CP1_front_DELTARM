ce98d059633e575a8323b9b22e2ac4d654956009
Nicolas Teysseyre <nicolas.teysseyre@deltarm.com>
2022-05-05 09:55:59 +0200

    feat: add dashboard of others client

--- src/Features/DashboardDynamic/components/DashboardDynamic.stories.tsx
+++ src/Features/DashboardDynamic/components/DashboardDynamic.stories.tsx
@@ -2,7 +2,11 @@ import React, { useEffect } from 'react';
 import { DashboardDynamic } from './DashboardDynamic';
 import { worker } from '../../../mocks/server';
 import { rest } from 'msw';
-import DASHBOARD from '../../../mocks/fixtures/dashboard/dashboard';
+import {
+  DASHBOARD_KLESIA,
+  DASHBOARD_BPI,
+  DASHBOARD_CHANTIER_ABC,
+} from '../../../mocks/fixtures/dashboard/dashboard';
 import { data as MODAL } from '../../../mocks/fixtures/modal/modal';
 import { RecoilRoot } from 'recoil';
 
@@ -24,12 +28,54 @@ const Template: any = (args: any) => {
   return <DashboardDynamic {...args} />;
 };
 
-export const Dashboard = Template.bind({});
-Dashboard.decorators = [
+export const DashboardBPI = Template.bind({});
+DashboardBPI.decorators = [
   (story: any) => {
     worker?.use(
       rest.get('https://undefined/dashboard/contr_perm', (req, res, ctx) => {
-        return res(ctx.status(200), ctx.json(DASHBOARD));
+        return res(ctx.status(200), ctx.json(DASHBOARD_BPI));
+      }),
+      rest.get(
+        'https://undefined/contr_perm/get_search_test',
+        (req, res, ctx) => {
+          req.url.searchParams.get('value');
+
+          return res(ctx.status(200), ctx.json(MODAL));
+        },
+      ),
+    );
+
+    return story();
+  },
+];
+
+export const DashboardKlesia = Template.bind({});
+DashboardKlesia.decorators = [
+  (story: any) => {
+    worker?.use(
+      rest.get('https://undefined/dashboard/contr_perm', (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json(DASHBOARD_KLESIA));
+      }),
+      rest.get(
+        'https://undefined/contr_perm/get_search_test',
+        (req, res, ctx) => {
+          req.url.searchParams.get('value');
+
+          return res(ctx.status(200), ctx.json(MODAL));
+        },
+      ),
+    );
+
+    return story();
+  },
+];
+
+export const DashboardChantierABC = Template.bind({});
+DashboardChantierABC.decorators = [
+  (story: any) => {
+    worker?.use(
+      rest.get('https://undefined/dashboard/contr_perm', (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json(DASHBOARD_CHANTIER_ABC));
       }),
       rest.get(
         'https://undefined/contr_perm/get_search_test',
