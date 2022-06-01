b30a5505944711b0a4f25234dae699b5c72e0bde
Nicolas Teysseyre <nicolas.teysseyre@deltarm.com>
2022-06-01 14:23:28 +0200

    feat: adapt compliance endpoint returns

--- src/Features/Edit/routes.ts
+++ src/Features/Edit/routes.ts
@@ -36,28 +36,7 @@ apiRouter.registerRoute({
   name: 'getCompliance',
   path: '/control/get_compliance_values',
   method: 'get',
-  handler: (response): IComplianceData[] => {
-    const data: IApiComplianceData[] = response.data;
-    const complianceElms: IComplianceData[] = [];
-
-    data.map((compliance) => {
-      const com: IComplianceData = {
-        desc1: compliance.compliance_elm_desc_1,
-        desc2: compliance.compliance_elm_desc_2,
-        family: compliance.compliance_elm_family,
-        id: compliance.compliance_id,
-        lib: compliance.compliance_elm_lib,
-        regex: compliance.compliance_elm_regex,
-        regexMsg: compliance.compliance_elm_regex_msg,
-        type: compliance.compliance_elm_type,
-        value: compliance.compliance_elm_value,
-        uploadDetail: compliance.compliance_file_detail,
-      };
-      complianceElms.push(com);
-
-      return compliance;
-    });
-
-    return complianceElms;
+  handler: (response): IApiComplianceData => {
+    return response.data;
   },
 });
