49f1bb6a601ea2760087e0d2a89ce5545a32a488
Nicolas Teysseyre <nicolas.teysseyre@deltarm.com>
2022-07-08 16:25:20 +0200

    feat: dynamic color title on card

--- src/Features/DashboardDynamic/components/Card/AgGridCard.stories.tsx
+++ src/Features/DashboardDynamic/components/Card/AgGridCard.stories.tsx
@@ -1393,7 +1393,7 @@ AgCardDashboard.args = {
     ],
     title: {
       bg_color: '#2ECC71',
-      font_color: '#FFFFFF',
+      font_color: 'blue',
       lib: 'Contracts in progress',
     },
   },
