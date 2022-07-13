83ef9a08e65efb5015036dfbe977a9f33bc4de68
Nicolas Teysseyre <nicolas.teysseyre@deltarm.com>
2022-07-13 11:30:02 +0200

    feat: add more fields in compliance dialog modal

--- src/Features/Edit/components/Control/Form/Compliance/Compliance.style.ts
+++ src/Features/Edit/components/Control/Form/Compliance/Compliance.style.ts
@@ -9,3 +9,41 @@ export const ActionsComplianceContainer = styled.div`
     margin: 5px;
   }
 `;
+
+export const FormComplianceContainer = styled.div`
+  .control-container {
+    position: relative;
+  }
+
+  .MuiGrid-item {
+    padding: 0.5rem;
+  }
+  ._FormError {
+    padding: 0;
+  }
+
+  ._FormLabel {
+    width: 100%;
+
+    .MuiGrid-item {
+      margin: 0;
+      padding: 0;
+
+      &:last-child {
+        height: 20px;
+      }
+    }
+  }
+
+  .control-footer {
+    margin-top: 2px;
+    position: absolute;
+    right: 0;
+    width: auto;
+
+    .MuiGrid-item {
+      margin: 0;
+      padding: 0;
+    }
+  }
+`;
