c492398c49b63c905f73c2aa3dfb90ff6f5245d2
asim <asimjaipuri96@gmail.com>
2022-08-09 13:50:38 +0530

    fix: implementing theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-
 import 'Services';
 import 'Features';
 import 'Shared';
 import 'Services/Api/registerCallState';
 import { security, SecurityProvider, useSecurity } from 'Services';
 import { MainContent, MainHeader } from './Shared/components';
+import jwtDecode from 'jwt-decode';
+import { BPITheme } from 'Styles';
+import { ThemeProvider } from 'styled-components/macro';
 
 interface AppProps {
   isEmbedded: boolean;
@@ -14,11 +16,17 @@ interface AppProps {
 const App: React.FC<AppProps> = ({ isEmbedded }): React.ReactElement => {
   const { user } = useSecurity();
 
+  const abc: any = user;
+
+  console.log(jwtDecode(abc?._jwt));
+
   return (
+    // <ThemeProvider theme={BPITheme}>
     <SecurityProvider security={security}>
       {user.isLogged() && !isEmbedded && <MainHeader />}
       <MainContent />
     </SecurityProvider>
+    // </ThemeProvider>
   );
 };
 
