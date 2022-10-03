5ab6d3c85e904396ce128734cea5df94401b2214
asim <asimjaipuri96@gmail.com>
2022-10-03 11:51:38 +0530

    fix: resolve dashboard conflicts

--- src/Features/Edit/components/EditValidation.tsx
+++ src/Features/Edit/components/EditValidation.tsx
@@ -41,9 +41,9 @@ export const EditValidation: React.FC<IProps> = ({
     logout();
   }
 
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
+  //   useEffect(() => {
+  //     console.log(data);
+  //   }, [data]);
 
   useEffect(() => {
     // To avoid (bpi specific)
