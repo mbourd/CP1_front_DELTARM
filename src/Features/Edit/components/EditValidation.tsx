b1fd3280e2cc9c8e344b390125e8a6f4520f7175
asim <asimjaipuri96@gmail.com>
2022-10-10 10:26:25 +0530

    feat: added section based conditional validation

--- src/Features/Edit/components/EditValidation.tsx
+++ src/Features/Edit/components/EditValidation.tsx
@@ -41,9 +41,9 @@ export const EditValidation: React.FC<IProps> = ({
     logout();
   }
 
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
+  //   useEffect(() => {
+  //     console.log(data);
+  //   }, [data]);
 
   useEffect(() => {
     // To avoid (bpi specific)
@@ -53,8 +53,6 @@ export const EditValidation: React.FC<IProps> = ({
     if (!isEmpty(frontRouterQueries)) {
       queries = frontRouterQueries;
     }
-
-    console.log(frontRouterQueries, apiRouteName);
 
     if (currentSection) {
       queries.section_id = currentSection;
