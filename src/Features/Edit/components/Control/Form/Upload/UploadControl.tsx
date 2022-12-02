df4b9f28792b74d9770b6dc60eb731ba5beb41af
asim9895 <asimjaipuri96@gmail.com>
2022-12-02 16:54:38 +0530

    fix: upload issue

--- src/Features/Edit/components/Control/Form/Upload/UploadControl.tsx
+++ src/Features/Edit/components/Control/Form/Upload/UploadControl.tsx
@@ -27,7 +27,7 @@ export const UploadControl: React.FC<IProps> = ({
   context,
 }): React.ReactElement => {
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
-  const [newUploadFile, setNewUploadFile] = useState<File | null>(null);
+  const [newUploadFile, setNewUploadFile] = useState<File | any>(null);
   const [currentUploadFile, setCurrentUploadFile] = useState<
     IUploadDetail[] | null
   >(control.upload_detail);
@@ -74,18 +74,18 @@ export const UploadControl: React.FC<IProps> = ({
     if (!control.mandatory) {
       setErrorMessage(null);
     }
-    if (newUploadFile !== null) {
-      uploadFile(
-        fileId,
-        control,
-        newUploadFile,
-        jwt,
-        setCurrentUploadFile,
-        setErrorMessage,
-        setNewUploadFile,
-      );
-    }
-    setNewUploadFile(null);
+    uploadFile(
+      fileId,
+      control,
+      newUploadFile,
+      jwt,
+      setCurrentUploadFile,
+      setErrorMessage,
+      setNewUploadFile,
+    );
+    setTimeout(() => {
+      setNewUploadFile(null);
+    }, 2000);
   }, [fileId, control, newUploadFile, jwt]);
 
   const handleDeleteFile = useCallback(
