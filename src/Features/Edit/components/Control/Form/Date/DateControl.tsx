6f0064fcb7e0b6068f072a9791caa346336a3f96
asim9895 <asimjaipuri96@gmail.com>
2023-01-30 19:24:32 +0530

    feat: added validation for date input

--- src/Features/Edit/components/Control/Form/Date/DateControl.tsx
+++ src/Features/Edit/components/Control/Form/Date/DateControl.tsx
@@ -28,24 +28,146 @@ export const DateControl: React.FC<IProps> = ({
   setFormState,
   context,
 }): React.ReactElement => {
-  // const today = new Date();
-  // const yesterday = new Date(today);
-  // const tomorrow = new Date(today);
+  const today = new Date();
+  const yesterday = new Date(today);
+  const tomorrow = new Date(today);
+  const day_after_tomorrow = new Date(today);
+  const next_month = new Date(today);
 
-  // yesterday.setDate(yesterday.getDate() - 1);
-  // tomorrow.setDate(tomorrow.getDate() + 1);
+  yesterday.setDate(yesterday.getDate() - 1);
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  day_after_tomorrow.setDate(day_after_tomorrow.getDate() + 2);
+  next_month.setDate(next_month.getMonth() + 2);
 
-  // const today_min_date = `${today.toLocaleDateString().split('/')[2]}-${
-  //   today.toLocaleDateString().split('/')[0]
-  // }-${today.toLocaleDateString().split('/')[1]}`;
+  const today_date = `${
+    today
+      .toLocaleString('en-GB', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      })
+      .split('/')[2]
+  }-${
+    today
+      .toLocaleString('en-GB', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      })
+      .split('/')[1]
+  }-${
+    today
+      .toLocaleString('en-GB', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      })
+      .split('/')[0]
+  }`;
 
-  // const yesterday_min_date = `${yesterday.toLocaleDateString().split('/')[2]}-${
-  //   yesterday.toLocaleDateString().split('/')[0]
-  // }-${yesterday.toLocaleDateString().split('/')[1]}`;
+  const yesterday_date = `${
+    yesterday
+      .toLocaleString('en-GB', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      })
+      .split('/')[2]
+  }-${
+    yesterday
+      .toLocaleString('en-GB', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      })
+      .split('/')[1]
+  }-${
+    yesterday
+      .toLocaleString('en-GB', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      })
+      .split('/')[0]
+  }`;
 
-  // const tomorrow_min_date = `${tomorrow.toLocaleDateString().split('/')[2]}-${
-  //   tomorrow.toLocaleDateString().split('/')[0]
-  // }-${tomorrow.toLocaleDateString().split('/')[1]}`;
+  const tomorrow_date = `${
+    tomorrow
+      .toLocaleString('en-GB', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      })
+      .split('/')[2]
+  }-${
+    tomorrow
+      .toLocaleString('en-GB', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      })
+      .split('/')[1]
+  }-${
+    tomorrow
+      .toLocaleString('en-GB', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      })
+      .split('/')[0]
+  }`;
+
+  const day_after_tomorrow_date = `${
+    day_after_tomorrow
+      .toLocaleString('en-GB', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      })
+      .split('/')[2]
+  }-${
+    day_after_tomorrow
+      .toLocaleString('en-GB', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      })
+      .split('/')[1]
+  }-${
+    day_after_tomorrow
+      .toLocaleString('en-GB', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      })
+      .split('/')[0]
+  }`;
+
+  const next_month_date = `${
+    next_month
+      .toLocaleString('en-GB', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      })
+      .split('/')[2]
+  }-${
+    next_month
+      .toLocaleString('en-GB', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      })
+      .split('/')[1]
+  }-${
+    next_month
+      .toLocaleString('en-GB', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      })
+      .split('/')[0]
+  }`;
 
   const { send, error } = useApi<void>();
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
@@ -189,7 +311,9 @@ export const DateControl: React.FC<IProps> = ({
           defaultValue={currentValue ? currentValue : ''}
           onBlur={(e) => saveValue(e.currentTarget.value)}
           type={'date'}
-          // InputProps={{ inputProps: { min: tomorrow_min_date } }}
+          InputProps={{
+            inputProps: { min: tomorrow_date, max: day_after_tomorrow_date },
+          }}
           // InputProps={{ inputProps: { min: '2022-05-04', max: '2022-05-22' } }}
         />
         {errorMessage ? <FormError>{errorMessage}</FormError> : null}
