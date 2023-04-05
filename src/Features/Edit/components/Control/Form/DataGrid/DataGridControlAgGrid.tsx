7858e38fa9068eb04b3b962f0084820c3abc055f
asimAivhub <asim_ext@aivhub.com>
2023-04-06 00:40:43 +0530

    created new custom_comment_rendered

--- src/Features/Edit/components/Control/Form/DataGrid/DataGridControlAgGrid.tsx
+++ src/Features/Edit/components/Control/Form/DataGrid/DataGridControlAgGrid.tsx
@@ -47,6 +47,7 @@ import CustomDateRenderer from './AgDataGridFields/CustomDateRenderer/CustomDate
 import CustomCheckBoxRenderer from './AgDataGridFields/CustomCheckBoxRenderer/CustomCheckBoxRenderer';
 import { EuroIcon } from 'Styles';
 import { minMax } from 'Packages/Helpers/src/minMax';
+import CustomCommentRenderer from './AgDataGridFields/CustomCommentRenderer/CustomCommentRenderer';
 
 const columns = [
   {
@@ -613,6 +614,15 @@ export const DataGridControlAgGrid: React.FC<IProps> = ({
             )}
             {props.value}
           </div>
+        );
+      /* Created new component for comment text */
+      case 'comment':
+        return (
+          <CustomCommentRenderer
+            props={props}
+            field_data={field_data}
+            control={control}
+          />
         );
       default:
         return props?.value ? props?.value : 'No value';
