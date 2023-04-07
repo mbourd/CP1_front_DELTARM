547a387045bf9d4dfc62c17cf389b87053b1eac3
asim9895 <asimjaipuri96@gmail.com>
2023-04-07 15:27:19 +0530

    feat: added custom select list

--- src/Features/Edit/components/Control/Form/DataGrid/AgDataGridFields/CustomSelectRenderer/CustomSelectRenderer.tsx
+++ src/Features/Edit/components/Control/Form/DataGrid/AgDataGridFields/CustomSelectRenderer/CustomSelectRenderer.tsx
@@ -1,43 +1,95 @@
 import React from 'react';
+import { saveValueDataGrid } from '../../apiRoutes/saveValueDataGrid';
+import {
+  CustomSelectRendererStyled,
+  OptionStyled,
+  CustomSelectStyled,
+} from './CustomSelectRenderer.style';
+import './style.css';
 
-const CustomSelectRenderer: React.FC<any> = ({ props, field_data }) => {
+const CustomSelectRenderer: React.FC<any> = ({
+  props,
+  field_data,
+  control,
+  fileId,
+  jwt,
+  seterrors,
+}) => {
+  const select_id_to_show = props?.colDef?.choice_options.filter(
+    (option: any) => {
+      return props.value?.toString() === option?.choice_id?.toString();
+    },
+  );
+
+  // console.log('defaultValue', select_id_to_show[0]?.choice_lib, props.value);
   const handleChange = (event: any) => {
-    props.setValue(event.target.value);
-    field_data.value = event.target.value;
+    const select_id = props?.colDef?.choice_options?.filter((option: any) => {
+      // console.log('showing before selecting', event?.target?.value, option);
+
+      return event?.target?.value === option?.choice_lib;
+    });
+    // console.log('value to save in grid', event?.target?.value, select_id);
+    props.setValue(select_id[0]?.choice_id?.toString());
+
+    // console.log('select_list', props, field_data);
+    // field_data.value = event.target.value;
+
+    saveValueDataGrid(
+      fileId,
+      control.control_id,
+      field_data?.col_elm_id,
+      field_data?.row_num,
+      jwt,
+      select_id[0]?.choice_id.toString(),
+      seterrors,
+      select_id[0]?.choice_id.toString(),
+    );
   };
 
   return (
-    <div
-      style={{
-        marginTop: 17,
-      }}
-    >
-      <select
-        value={props.value}
-        onChange={handleChange}
-        style={{
-          borderWidth: 0,
-          backgroundColor: 'transparent',
-          fontSize: 14,
-          padding: 10,
-        }}
-      >
-        {field_data?.answer_choices.map((option: any) => (
-          <option
-            key={option.id}
-            value={option.value}
-            style={{
-              padding: 10,
-              margin: 10,
-              paddingBottom: 30,
-              marginBottom: 30,
-            }}
-          >
-            {option.label}
-          </option>
-        ))}
-      </select>
-    </div>
+    <CustomSelectRendererStyled>
+      <div>
+        <CustomSelectStyled
+          defaultValue={select_id_to_show[0]?.choice_lib}
+          onChange={handleChange}
+          style={{
+            borderWidth: 0,
+            backgroundColor: 'transparent',
+            fontSize: 14,
+            color: field_data?.choice_font_color
+              ? field_data?.choice_font_color
+              : 'black',
+            fontWeight: field_data?.choice_font_weight
+              ? field_data?.choice_font_weight
+              : 'normal',
+            // padding: 10,
+          }}
+        >
+          {field_data?.choice_options?.map((option: any) => (
+            <OptionStyled
+              key={option.choice_id}
+              value={option.choice_lib}
+              className="custom-option"
+              aria-setsize={13}
+              style={{
+                fontSize: 14,
+                color: option?.choice_font_color
+                  ? option?.choice_font_color
+                  : 'black',
+                backgroundColor: option?.choice_bg_color
+                  ? option?.choice_bg_color
+                  : 'white',
+                fontWeight: option?.choice_font_weight
+                  ? option?.choice_font_weight
+                  : 'normal',
+              }}
+            >
+              {option.choice_lib}
+            </OptionStyled>
+          ))}
+        </CustomSelectStyled>
+      </div>
+    </CustomSelectRendererStyled>
   );
 };
 
