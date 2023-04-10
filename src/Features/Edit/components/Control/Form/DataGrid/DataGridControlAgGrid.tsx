5c52117c7cc20aa7555d5ef970c5598af7139df0
asim9895 <asimjaipuri96@gmail.com>
2023-04-10 18:47:08 +0530

    feat: added cell row styling

--- src/Features/Edit/components/Control/Form/DataGrid/DataGridControlAgGrid.tsx
+++ src/Features/Edit/components/Control/Form/DataGrid/DataGridControlAgGrid.tsx
@@ -111,7 +111,17 @@ export const DataGridControlAgGrid: React.FC<IProps> = ({
               },
             };
           default:
-            return g;
+            return {
+              ...g,
+              cellStyle: (params: any) => {
+                console.log(control);
+                return {
+                  backgroundColor: 'red',
+                  fontSize: 20,
+                  fontWeight: 'bold',
+                };
+              },
+            };
         }
       }),
     [control.data_grid_detail?.columns],
