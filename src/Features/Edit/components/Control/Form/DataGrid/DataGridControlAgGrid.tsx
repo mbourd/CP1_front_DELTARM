7b142d032097d80fd72368b7db6f48ecd823ea2e
asim9895 <asimjaipuri96@gmail.com>
2023-04-10 17:41:00 +0530

    feat: added styling to ag-grid

--- src/Features/Edit/components/Control/Form/DataGrid/DataGridControlAgGrid.tsx
+++ src/Features/Edit/components/Control/Form/DataGrid/DataGridControlAgGrid.tsx
@@ -83,6 +83,7 @@ export const DataGridControlAgGrid: React.FC<IProps> = ({
                     return data?.value === option?.choice_id?.toString();
                   },
                 );
+
                 return {
                   backgroundColor: select_id[0]?.option_bg_color,
                 };
