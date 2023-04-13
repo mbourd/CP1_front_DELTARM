7d8721ecfe8e65b96270e29173b666b41104681a
asimAivhub <asim_ext@aivhub.com>
2023-04-14 01:52:20 +0530

    select list styling and pagination numbers

--- src/Features/Edit/components/Control/Form/DataGrid/AgDataGridFields/CustomSelectRenderer/CustomSelectRenderer.tsx
+++ src/Features/Edit/components/Control/Form/DataGrid/AgDataGridFields/CustomSelectRenderer/CustomSelectRenderer.tsx
@@ -47,7 +47,7 @@ const CustomSelectRenderer: React.FC<any> = ({
   return (
     <CustomSelectRendererStyled>
       <div>
-        <CustomSelectStyled
+        {/* <CustomSelectStyled
           defaultValue={select_id_to_show[0]?.choice_lib}
           onChange={handleChange}
           fontSize={
@@ -72,7 +72,50 @@ const CustomSelectRenderer: React.FC<any> = ({
               value={option.choice_lib}
               className="custom-option"
               style={{
-                fontSize: 11,
+                padding: '2px !important',
+                fontSize: 25,
+                color: 'black',
+                // backgroundColor: option?.choice_bg_color
+                //   ? option?.choice_bg_color
+                //   : 'white',
+                backgroundColor: 'red',
+                fontWeight: option?.choice_font_weight
+                  ? option?.choice_font_weight
+                  : 'normal',
+              }}
+            >
+              &nbsp; {option.choice_lib}
+            </OptionStyled>
+          ))}
+        </CustomSelectStyled> */}
+        <CustomSelectStyled
+          value={select_id_to_show[0]?.choice_lib}
+          onChange={handleChange}
+          displayEmpty
+          inputProps={{ 'aria-label': 'Without label' }}
+          sx={{
+            fontSize: `${control?.data_grid_detail?.datagrid_options?.datagrid_font_size}px`,
+            borderWidth: 0,
+            backgroundColor: 'transparent',
+            color: control?.data_grid_detail?.datagrid_options
+              ?.datagrid_font_color
+              ? control?.data_grid_detail?.datagrid_options?.datagrid_font_color
+              : 'black',
+            fontWeight: field_data?.choice_font_weight
+              ? field_data?.choice_font_weight
+              : 'normal',
+
+            border: 0,
+          }}
+        >
+          {field_data?.choice_options?.map((option: any) => (
+            <OptionStyled
+              key={option.choice_id}
+              value={option.choice_lib}
+              className="custom-option"
+              sx={{
+                padding: 5,
+                fontSize: 10,
                 color: 'black',
                 backgroundColor: option?.choice_bg_color
                   ? option?.choice_bg_color
@@ -82,7 +125,7 @@ const CustomSelectRenderer: React.FC<any> = ({
                   : 'normal',
               }}
             >
-              &nbsp; {option.choice_lib}
+              {option.choice_lib}
             </OptionStyled>
           ))}
         </CustomSelectStyled>
