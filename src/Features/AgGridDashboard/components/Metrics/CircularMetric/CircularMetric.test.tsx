5296be83ffc4462639c9edc42217b41d5d9f7191
asim9895 <asimjaipuri96@gmail.com>
2023-05-02 13:09:47 +0530

    fix: resolved conflicts

---
+++ src/Features/AgGridDashboard/components/Metrics/CircularMetric/CircularMetric.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import * as stories from './CircularMetric.stories';
+import { composeStories } from '@storybook/testing-react';
+import { render } from 'setupTests';
+const { Circular } = composeStories(stories);
+
+describe('Metric', () => {
+  describe('Metric', () => {
+    test('Should render the circular metric', async () => {
+      const { getByRole } = render(<Circular />);
+      expect(getByRole('progressbar')).toBeInTheDocument();
+      expect(getByRole('progressbar')).toHaveAttribute('role', 'progressbar');
+      expect(getByRole('progressbar')).toHaveAttribute(
+        'title',
+        'Taux de Complétude : 54 dossiers / 100 prévus au plan = 54%',
+      );
+      expect(getByRole('progressbar')).toHaveAttribute('aria-valuenow', '74');
+    });
+  });
+});
