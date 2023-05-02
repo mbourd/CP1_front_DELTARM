e24f67869ce5958d2dfdc9c1fb6e2a7634ffb7eb
asim9895 <asimjaipuri96@gmail.com>
2023-05-02 13:09:47 +0530

    feat: added ag-grid dashboard

---
+++ src/Features/AgGridDashboard/components/Card/Header/Header.style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components/macro';
+import { headerHeight, ICardHeader } from '../types';
+
+export const HeaderStyled = styled.header<Omit<ICardHeader, 'children'>>`
+  background-color: ${({ color }) => color};
+  color: ${({ theme }) => theme.color.white.main};
+  font-family: ${({ theme }) => theme.font.medium.main};
+  height: ${headerHeight}px;
+  line-height: ${headerHeight}px;
+  text-align: center;
+  text-transform: uppercase;
+`;
---
+++ src/Features/AgGridDashboard/components/Search/Modal/GenerateFieldManual.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { Control, Controller } from 'react-hook-form';
+import { FormLabel, Select } from '../../../../../Packages/Design/components';
+import { Input } from '@material-ui/core';
+
+interface IProps {
+  field: any;
+  handleLeaveField: (event: any) => void;
+  setListMissingField: (values: Record<string, true>, key: any) => void;
+  control: Control<Record<string, any>>;
+}
+export const GenerateFieldManual: React.FC<IProps> = ({
+  field,
+  handleLeaveField,
+  setListMissingField,
+  control,
+}): React.ReactElement | null => {
+  switch (field.type) {
+    case 'select_list':
+      return (
+        <Controller
+          render={() => (
+            <div className={'missing-fields'} key={field.id}>
+              <FormLabel>{field.label}</FormLabel>
+              <Select
+                name={field.key}
+                data={field.option}
+                multiple={false}
+                selectedValues={{
+                  [Object.keys(field.option)[0] || '-1']: true,
+                }}
+                onInit={setListMissingField}
+                onClose={setListMissingField}
+                closeOnSelect
+              >
+                {field.label}
+              </Select>
+            </div>
+          )}
+          rules={{
+            pattern: field.format,
+          }}
+          control={control}
+          name={field.key}
+          key={field.order}
+          defaultValue={''}
+        />
+      );
+    case 'float':
+      return (
+        <Controller
+          render={() => (
+            <div className={'missing-fields'} key={field.id}>
+              <FormLabel>{field.label}</FormLabel>
+              <div className={'missing-field'}>
+                <Input
+                  name={field.key}
+                  type={'number'}
+                  onChange={handleLeaveField}
+                  placeholder={field.label}
+                />
+              </div>
+            </div>
+          )}
+          rules={{
+            pattern: field.format,
+          }}
+          control={control}
+          name={field.key}
+          key={field.order}
+          defaultValue={''}
+        />
+      );
+    case 'string':
+      return (
+        <Controller
+          render={() => (
+            <div className={'missing-fields'} key={field.id}>
+              <FormLabel>{field.label}</FormLabel>
+              <div className={'missing-field'}>
+                <Input
+                  name={field.key}
+                  type={'text'}
+                  onChange={handleLeaveField}
+                  placeholder={field.label}
+                />
+              </div>
+            </div>
+          )}
+          rules={{
+            pattern: field.format,
+          }}
+          control={control}
+          name={field.key}
+          key={field.order}
+          defaultValue={''}
+        />
+      );
+  }
+
+  return null;
+};
