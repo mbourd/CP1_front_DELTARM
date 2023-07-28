91aec16b362e4bccc8e1b2f37e85ed911e131a38
Maxime Bourdin <maxime.bourdin@deltarm.com>
2023-07-28 10:33:26 +0200

    fix: rename props styled comps

--- src/Features/Edit/components/Control/Form/DataGrid/AgDataGridFields/CustomFinancialRenderer/CustomFinancialRendererSpan.style.ts
+++ src/Features/Edit/components/Control/Form/DataGrid/AgDataGridFields/CustomFinancialRenderer/CustomFinancialRendererSpan.style.ts
@@ -1,11 +1,11 @@
 import styled from 'styled-components/macro';
 
 const CustomFinancialSpanStyled = styled.span<{
-  font_size: string;
-  margin_right: string;
+  $font_size: string;
+  $margin_right: string;
 }>`
-  font-size: ${({ font_size }) => font_size};
-  margin-right: ${({ margin_right }) => margin_right};
+  font-size: ${({ $font_size }) => $font_size};
+  margin-right: ${({ $margin_right }) => $margin_right};
 `;
 
 export { CustomFinancialSpanStyled };
