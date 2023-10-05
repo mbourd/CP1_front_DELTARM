4dc4b4b23bedb37e8f6f7e12a8ca6f210d0fa1e6
Maxime Bourdin <maxime.bourdin@deltarm.com>
2023-10-05 11:13:28 +0200

    fix: save value compliances even if mandatory

--- src/Features/Edit/components/Control/Form/Compliance/FormCompliance/DateCompliance/DateCompliance.tsx
+++ src/Features/Edit/components/Control/Form/Compliance/FormCompliance/DateCompliance/DateCompliance.tsx
@@ -29,12 +29,6 @@ export const DateCompliance: React.FC<IProps> = ({
 
   const saveValue = useCallback(
     (value: string) => {
-      if (!value && isMandatory) {
-        setErrorMessage(trans('mandatoryValue'));
-
-        return;
-      }
-
       if (
         compliance.compliance_elm_regex &&
         !value.match(compliance.compliance_elm_regex)
@@ -65,8 +59,6 @@ export const DateCompliance: React.FC<IProps> = ({
       compliance.compliance_elm_regex,
       compliance.compliance_id,
       compliance.compliance_elm_regex_msg,
-      isMandatory,
-      trans,
     ],
   );
 
