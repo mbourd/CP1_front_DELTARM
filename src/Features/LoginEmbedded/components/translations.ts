70f52348604325128f040bcd541709b27d7db23a
Maxime Bourdin <maxime.bourdin@deltarm.com>
2024-02-01 10:38:58 +0100

    feat: add custom hooks for translations

--- src/Features/LoginEmbedded/components/translations.ts
+++ src/Features/LoginEmbedded/components/translations.ts
@@ -1,18 +1,41 @@
-import { translation } from 'Services';
+import { translation, useTrans } from 'Services';
+import { useCallback } from 'react';
 
-translation.addBatchResource({
-  Login: {
-    pageTitle: {
-      en: 'Logging in...',
-      fr: 'Connexion en cours...',
-    },
-    errorTitle: {
-      en: "Couldn't log in",
-      fr: "L'authentification a échoué",
-    },
-    logoutButton: {
-      en: 'Logout',
-      fr: 'Déconnexion',
-    },
+const namespace = 'Login';
+const transLogin = {
+  pageTitle: {
+    en: 'Logging in...',
+    fr: 'Connexion en cours...',
+    de: 'Einloggen...',
   },
+  errorTitle: {
+    en: "Couldn't log in",
+    fr: "L'authentification a échoué",
+    de: 'Anmeldung nicht möglich',
+  },
+  logoutButton: {
+    en: 'Logout',
+    fr: 'Déconnexion',
+    de: 'Ausloggen',
+  },
+};
+translation.addBatchResource({
+  [namespace]: transLogin,
 });
+type UseTransLoginReturnType = {
+  trans: (key: keyof typeof transLogin, options?: Record<any, any>) => string;
+  transLogin: typeof transLogin;
+  currentLang: string | undefined;
+  changeLang: (lang: string) => void;
+};
+export const useTransLoginEmbedded = (): UseTransLoginReturnType => {
+  const [trans, changeLang, currentLang] = useTrans(namespace);
+  const translate = useCallback(
+    (key: keyof typeof transLogin, options?: any): string => {
+      return trans(key, options);
+    },
+    [trans],
+  );
+
+  return { trans: translate, transLogin, currentLang, changeLang };
+};
