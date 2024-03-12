202da1bd7919447f4cf7ad707b644152479f81f3
Maxime Bourdin <maxime.bourdin@deltarm.com>
2024-03-12 17:27:04 +0100

    fix: error msg when upload fie fails

--- src/Features/Edit/components/Control/Form/Upload/apiRoutes/uploadFile.ts
+++ src/Features/Edit/components/Control/Form/Upload/apiRoutes/uploadFile.ts
@@ -37,7 +37,7 @@ export const uploadFile = (
       return setCurrentUploadFile(res.data.data.file_detail);
     })
     .catch((err) => {
-      if (err.response.data.error_msg) {
+      if (err?.response?.data?.error_msg) {
         return setErrorMessage(err?.response?.data?.error_msg);
       }
     });
