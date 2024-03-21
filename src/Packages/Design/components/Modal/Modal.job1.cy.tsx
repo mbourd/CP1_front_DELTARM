eff8773a123505d6bece165318713ff788e093d6
Maxime Bourdin <maxime.bourdin@deltarm.com>
2024-03-21 12:45:10 +0100

    fix: adjust height for textarea import overview

--- src/Packages/Design/components/Modal/Modal.job1.cy.tsx
+++ src/Packages/Design/components/Modal/Modal.job1.cy.tsx
@@ -3,7 +3,7 @@
 /// <reference types="../../../../../cypress/support/component" />
 
 // NOTE: Run CLI:
-// yarn cypress:run:component --browser chrome --config video=false --spec "src/Packages/Design/components/Modal/Modal.cy.tsx"
+// yarn cypress:run:component --browser chrome --config video=false --spec "src/Packages/Design/components/Modal/Modal.job1.cy.tsx"
 
 import React from 'react';
 import { SetupTestsComponents } from '../../../../../cypress/utils/SetupTestsComponents';
